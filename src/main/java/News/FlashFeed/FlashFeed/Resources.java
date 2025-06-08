eefa06bfb47feed8656a1ee9ca843f8a078101fd
deeksha <deeksha.jindal@olik.ai>
2025-06-08 19:22:47 +0530

    gid

--- src/main/java/News/FlashFeed/FlashFeed/Resources.java
+++ src/main/java/News/FlashFeed/FlashFeed/Resources.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class Resources {
     private String id;
     private String name;
-    private String description;
+    private String description ;
     private String url;
     private String category;
     private String language;
